4af2166010ca6ddebeb89530e71613edec55e03a
Keith Carangelo <mail@kcaran.com>
2025-01-09 21:16:59 -0500

    Completed day11

--- day11.clj
+++ day11.clj
@@ -2,13 +2,19 @@
 ;
 ; day11.clj $(cat test11.txt)
 ;
+; I did get a little help from Reddit, but still had to create the memoization
+;
 
 (ns day11
 (:require [clojure.string :refer [split trim]])
 (:require [myutils :refer [read-input]])
 )
 
-(defn solve [stone blinks]
+(def memo-cache (atom {}))
+
+
+(defn calc-solve [stone blinks]
+  (declare solve)
 (println stone blinks)
   (let [ss (str stone) len (count ss)]
   (cond
@@ -24,9 +30,22 @@
   )
 )
 
+(defn solve [stone blinks]
+  (if-let [result (get @memo-cache [stone blinks])]
+    result
+    (let [result (calc-solve stone blinks)]
+      (swap! memo-cache assoc [stone blinks] result)
+      result
+    ) 
+  )
+)
+
 (defn -main [& args]
-  (println "The number of stones is "
+  (println "The number of stones for part a is "
     (reduce + (map (fn [x] (solve (Integer. x) 25)) args))
+  )
+  (println "The number of stones for part b is "
+    (reduce + (map (fn [x] (solve (Integer. x) 75)) args))
   )
 )
 
