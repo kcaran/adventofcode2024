de31029368b628d9f9a8233439e37ce0352f8a53
Keith Carangelo <mail@kcaran.com>
2025-01-11 17:11:48 -0500

    Completed day 12

--- day12.clj
+++ day12.clj
@@ -5,6 +5,93 @@
 (:require [myutils :refer [read-input]])
 )
 
+(defn c-nw [lmap pos]
+  (let [top (zero? (pos 0)) left (zero? (pos 1))]
+  (cond
+    ; Top Row
+    top (or left
+      (not= (get-in lmap [0 (dec (pos 1))]) (get-in lmap pos)))
+    ; First column
+    left
+      (not= (get-in lmap [(dec (pos 0)) 0]) (get-in lmap pos))
+    :else
+      (or
+        (and (not= (get-in lmap [(dec (pos 0)) (pos 1)]) (get-in lmap pos))
+           (not= (get-in lmap [(pos 0) (dec (pos 1))]) (get-in lmap pos)))
+        (and (= (get-in lmap [(dec (pos 0)) (pos 1)]) (get-in lmap pos))
+           (= (get-in lmap [(pos 0) (dec (pos 1))]) (get-in lmap pos))
+           (not= (get-in lmap [(dec (pos 0)) (dec (pos 1))]) (get-in lmap pos)))
+      )
+  )
+  )
+)
+
+(defn c-ne [lmap pos]
+  (let [top (zero? (pos 0)) right (= (pos 1) (dec (count lmap)))]
+  (cond
+    ; Top Row
+    top (or right
+      (not= (get-in lmap [0 (inc (pos 1))]) (get-in lmap pos)))
+    ; Last column
+    right
+      (not= (get-in lmap [(dec (pos 0)) (pos 1)]) (get-in lmap pos))
+    :else
+      (or
+        (and (not= (get-in lmap [(dec (pos 0)) (pos 1)]) (get-in lmap pos))
+           (not= (get-in lmap [(pos 0) (inc (pos 1))]) (get-in lmap pos)))
+        (and (= (get-in lmap [(dec (pos 0)) (pos 1)]) (get-in lmap pos))
+             (= (get-in lmap [(pos 0) (inc (pos 1))]) (get-in lmap pos))
+             (not= (get-in lmap [(dec (pos 0)) (inc (pos 1))]) (get-in lmap pos))
+        )
+      )
+  )
+  )
+)
+
+(defn c-sw [lmap pos]
+  (let [bot (= (pos 0) (dec (count lmap))) left (zero? (pos 1))]
+  (cond
+    ; Bottom Row
+    bot (or left
+      (not= (get-in lmap [(pos 0) (dec (pos 1))]) (get-in lmap pos)))
+    ; First column
+    left
+      (not= (get-in lmap [(inc (pos 0)) 0]) (get-in lmap pos))
+    :else
+      (or
+        (and (not= (get-in lmap [(inc (pos 0)) (pos 1)]) (get-in lmap pos))
+           (not= (get-in lmap [(pos 0) (dec (pos 1))]) (get-in lmap pos)))
+        (and (= (get-in lmap [(inc (pos 0)) (pos 1)]) (get-in lmap pos))
+           (= (get-in lmap [(pos 0) (dec (pos 1))]) (get-in lmap pos))
+           (not= (get-in lmap [(inc (pos 0)) (dec (pos 1))]) (get-in lmap pos))
+        )
+      )
+  )
+  )
+)
+
+(defn c-se [lmap pos]
+  (let [bot (= (pos 0) (dec (count lmap))) right (= (pos 1) (dec (count lmap)))]
+  (cond
+    ; Bottom Row
+    bot (or right
+      (not= (get-in lmap [(pos 0) (inc (pos 1))]) (get-in lmap pos)))
+    ; Last column
+    right
+      (not= (get-in lmap [(inc (pos 0)) (pos 1)]) (get-in lmap pos))
+    :else
+      (or
+        (and (not= (get-in lmap [(inc (pos 0)) (pos 1)]) (get-in lmap pos))
+           (not= (get-in lmap [(pos 0) (inc (pos 1))]) (get-in lmap pos)))
+        (and (= (get-in lmap [(inc (pos 0)) (pos 1)]) (get-in lmap pos))
+           (= (get-in lmap [(pos 0) (inc (pos 1))]) (get-in lmap pos))
+           (not= (get-in lmap [(inc (pos 0)) (inc (pos 1))]) (get-in lmap pos))
+        )
+      )
+  )
+  )
+)
+
 (defn f-up [lmap pos]
   (if (or (zero? (pos 0))
            (not= (get-in lmap [(dec (pos 0)) (pos 1)]) (get-in lmap pos)))
@@ -73,6 +160,14 @@
   )
 )
 
+(def has-corner [
+   c-ne
+   c-se
+   c-sw
+   c-nw
+  ]
+)
+
 (def has-fence [
    f-up
    f-down
@@ -134,10 +229,24 @@
   )
 )
 
+(defn corners [lmap region]
+  (reduce +
+    (map (fn [pos] (reduce + (map (fn [i] (if ((has-corner i) lmap pos) 1 0)) (range 4)))) region)
+  )
+)
+
 (defn calc-price [lmap]
   (let [regions (traverse lmap)]
   (reduce +
     (map (fn [r] (* (fence lmap r) (count r))) regions)
+  )
+  )
+)
+
+(defn calc-disc [lmap]
+  (let [regions (traverse lmap)]
+  (reduce +
+    (map (fn [r] (* (corners lmap r) (count r))) regions)
   )
   )
 )
@@ -151,6 +260,7 @@
 (defn -main [& args]
   (let [lmap (parse (myutils/read-input (first args)))]
     (println "The price of fences is " (calc-price lmap))
+    (println "The price of fences with the discount is " (calc-disc lmap))
   )
 )
 
